3825b141d7c77cf5157f60378ecc3dfc0268fbb4
44075.longroad.ac.uk <44075@longroad.ac.uk>
2014-10-08 19:16:47 +0100

    iuhgwi

---
+++ Convert a text character to its equivalent ASCII code.py
@@ -0,0 +1,8 @@
+#Callum Brown
+#Convert a text character to its equivalent ASCII code
+
+num = int(input("Please input an ascii number: "))
+character=chr(num)
+print("The character is a {0}".format(character))
+
+
---
+++ Text character to equivalent ASCII code.py
@@ -0,0 +1,9 @@
+#Callum Brown
+#Text character to equivalent ASCII code
+
+
+
+char= input("Please enter a character: ")
+ascii1 =ord(char)
+print("The ascii number is {0}".format(ascii1))
+
